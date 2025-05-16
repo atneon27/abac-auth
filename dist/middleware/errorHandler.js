0f1ff164f580915ed2a3e341317a2ce7210c4d9b
atneon27 <132840385+atneon27@users.noreply.github.com>
2025-05-16 23:30:16 +0530

    Delete dist directory

--- dist/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res) => {
-    console.log(err.stack);
-    res.status(500).json({
-        message: "Internal Server Error",
-        data: null,
-        error: err.message
-    });
-};
-export default errorHandler;
--- dist/routes/projectRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import verifyToken from "../middleware/authentication.js";
-import { viewProject, updateProject } from "../controllers/projectController.js";
-const router = Router();
-router.get('/:id', verifyToken, viewProject);
-router.post('/:id', verifyToken, updateProject);
-export default router;
